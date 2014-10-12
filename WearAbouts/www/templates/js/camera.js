b2e3c944bba7e74f49f9048ae37e2fe4935f8713
karltayeb <karl.tayeb7@gmail.com>
2014-10-12 08:50:21 -0400

    takes 2 pix

--- WearAbouts/www/templates/js/camera.js
+++ WearAbouts/www/templates/js/camera.js
@@ -5,45 +5,55 @@ $(document).ready(function() {
 	if (!user) {
 	  window.location.replace("login.html");
 	}
-
+	function encodeImageUri(imageUri)
+	{
+	     var c=document.createElement('canvas');
+	     var ctx=c.getContext("2d");
+	     var img=new Image();
+	     img.onload = function(){
+	       c.width=this.width;
+	       c.height=this.height;
+	       ctx.drawImage(img, 0,0);
+	     };
+	     img.src=imageUri;
+	     var dataURL = c.toDataURL("image/jpeg");
+	     return dataURL;
+	}
 	user = Parse.User;
 
 	var parseFile1;
 	$("#camera_shot1").click(function() {
-		var base64image;
 		navigator.camera.getPicture(
 			function(imageData) {
-				base64image = imageData;
+				document.getElementById('pic1').src = encodeImageUri(imageData);
+    			//var imageBase64 = imageData.replace(/^data:image\/(png|jpeg);base64,/, "");
+    			parseFile1 = new Parse.File("UploadOne", {base64:imageData});
 			},
 			function(message){
 				alert("Something went wrong. Camera could not be opened.");
 			},
-			{}
+			{DestinationType:Camera.DestinationType.FILE_URI}
 		);
-
-		var imageBase64 = base64image.replace(/^data:image\/(png|jpeg);base64,/, "");
-		var parseFile1 = new Parse.File(fileName, {base64:imageBase64});
 		
-		document.getElementById("camera_shot1").innerHTML = "Picture 1 Taken";
+		
+		$('#camera_text1').text('Picture Taken');
 	});
 
 	var parseFile2;
 	$("#camera_shot2").click(function() {
-		var base64image;
 		navigator.camera.getPicture(
 			function(imageData) {
-				base64image = imageData;
+				document.getElementById('pic2').src = encodeImageUri(imageData);
+    			//var imageBase64 = imageData.replace(/^data:image\/(png|jpeg);base64,/, "");
+    			parseFile2 = new Parse.File("UploadTwo", {base64:imageData});
 			},
 			function(message){
 				alert("Something went wrong. Camera could not be opened");
 			},
-			{}
+			{DestinationType:Camera.DestinationType.DATA_URL}
 		);
-
-		var imageBase64 = base64image.replace(/^data:image\/(png|jpeg);base64,/, "");
-		var parseFile2 = new Parse.File(fileName, {base64:imageBase64});
 		
-		document.getElementById("camera_shot2").innerHTML = "Picture 2 Taken";
+		$('#camera_text2').text('Picture Taken');
 	});
 
 	$("#picture_submit").click(function() {
