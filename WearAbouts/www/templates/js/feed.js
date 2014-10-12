426e3c8ef4fa963e9844be3e22a4518870975d74
airrick213 <erickim213@gmail.com>
2014-10-12 11:37:23 -0400

    feed fix

--- WearAbouts/www/templates/js/feed.js
+++ WearAbouts/www/templates/js/feed.js
@@ -20,9 +20,40 @@ $(document).ready(function() {
 		window.location.replace("login.html");
 	});
 
+	var query = new Parse.Query(Parse.User);
+	var random_user;
+	function random_user_post() {
+		alert("hello");
+		query.exists("username");
+		query.find({
+		  success: function(results) {
+		  	var userList = results;
+		    var randomIndex = Math.round(Math.random() * userList.length);
+		    random_user = userList[randomIndex];
+			var first_picture = random_user.get("picture_pair1a");
+			var second_picture = random_user.get("picture_pair1b");
+			$("#pic1").src = first_picture.url();
+			$("#pic2").src = second_picture.url();
+			alert(random_user.get("username"));
+		  },
+		  error: function(error) {
+		    alert("Error: " + error.code + " " + error.message);
+		  }
+		});
+	}
 
-	var userList = Parse.Query(Parse.User);
-	var randomIndex = Math.random() * userList.length;
-	console.log(userList[randomIndex]);
-
+	random_user_post();
+	$("#picture1").click(function() {
+		random_user.set(random_user.get("vote_a") + 1);
+		random_user.save();
+		random_user_post();
+	});
+	$("#picture2").click(function() {
+		random_user.set(random_user.get("vote_b") + 1);
+		random_user.save();
+		random_user_post();
+	});
+	$("#neither").click(function() {
+		random_user_post();
+	}
 });
